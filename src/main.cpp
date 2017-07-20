ba4f409094e5b597b5f5022787d63af2d4c8600f
Kate Isaacs <kisaacs@cs.arizona.edu>
2017-07-20 15:21:52 -0700

    More changes attempting to make build work.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <iostream>
 #include "trace.h"
 #include "importfunctor.h"
-#include "../lib/external/mongoose.h"
+#include "external/mongoose.h"
 
 static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
